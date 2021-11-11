42b5ab96ca0db4e49c87b7398dfbe2a45f033c7e
agnetachow <1498142578@qq.com>
2021-11-11 17:04:01 +0800

    2021-11-11

--- src/main/resources/static/js/offcanvas.js
+++ src/main/resources/static/js/offcanvas.js
@@ -1,5 +1,10 @@
-$(document).ready(function () {
-  $('[data-toggle="offcanvas"]').click(function () {
-    $('.row-offcanvas').toggleClass('active')
+$(document).ready(function(){
+  $(".add").click(function(){
+  $(".tip").fadeIn(200);
   });
+
+  $(".tip_title a").click(function(){
+  $(".tip").fadeOut(200);
+});
+
 });
